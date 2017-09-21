f74725bf143ca581f37b3682ff856d656eea880d
talhauddin <talha.udin@gmail.com>
2017-09-22 00:20:06 +0500

    Sign Out

--- SindhTV_Web/application/views/main_layouts/header.php
+++ SindhTV_Web/application/views/main_layouts/header.php
@@ -2,9 +2,9 @@
     <!-- Logo -->
     <a href="index2.html" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini"><b>A</b>LT</span>
+      <span class="logo-mini"><b>S</b>TV</span>
       <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b>Admin</b>LTE</span>
+      <span class="logo-lg"><b>Sindh</b>TV</span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
@@ -249,7 +249,7 @@
                   <a href="#" class="btn btn-default btn-flat">Profile</a>
                 </div>
                 <div class="pull-right">
-                  <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                  <a href="<?php echo base_url();?>index.php/auth/logout" class="btn btn-default btn-flat">Sign out</a>
                 </div>
               </li>
             </ul>
